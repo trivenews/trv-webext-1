87f0ddf6fe240b7bab95a5d6c72847500d41aef4
devkabiir <dev@kabiir.me>
2018-09-04 04:25:10 +0530

    test(background): simplify tests for screenshot actions
    removes redundant screenshot tests

--- test/specs/background/background.spec.ts
+++ test/specs/background/background.spec.ts
@@ -1,11 +1,46 @@
 import '@/background';
+import * as sinon from 'sinon';
+
 describe('background page', () => {
-  beforeAll(() => {
-    jest.useFakeTimers();
+  beforeEach(() => {
+    browser.tabs.captureVisibleTab.resetHistory();
+    // return a dummy image
+    browser.tabs.captureVisibleTab.resolves(
+      // tslint:disable-next-line:max-line-length
+      'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAADElEQVQImWNgoBMAAABpAAFEI8ARAAAAAElFTkSuQmCC',
+    );
+
+    const createObjectURLMock = sinon.mock();
+    createObjectURLMock.returns('');
+    URL.createObjectURL = createObjectURLMock;
+
+    // return a download id
+    browser.downloads.download.resolves(1);
   });
-  afterAll(() => jest.useRealTimers());
 
   it('should set up a listener', () => {
     expect(browser.runtime.onMessage.addListener.calledOnce).toBe(true);
+  });
+
+  it('should capture and save screenshot', () => {
+    browser.runtime.onMessage.dispatch({ action: 'capture-and-save' });
+
+    expect(browser.tabs.captureVisibleTab.calledOnce).toBe(true);
+    // fake awaiting
+    process.nextTick(() => {
+      expect(browser.downloads.download.calledOnce).toBe(true);
+    });
+  });
+
+  it('should capture screenshot', () => {
+    browser.runtime.onMessage.dispatch({ action: 'capture-only' });
+
+    expect(browser.tabs.captureVisibleTab.calledOnce).toBe(true);
+  });
+
+  it('should save screenshot', () => {
+    browser.runtime.onMessage.dispatch({ action: 'save-only' });
+
+    expect(browser.downloads.download.calledOnce).toBe(true);
   });
 });
