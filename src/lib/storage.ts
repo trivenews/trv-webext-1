e324a5ab3fa73219151adcb35f39fd13a3c7f9a7
devkabiir <dev@kabiir.me>
2018-09-04 21:55:55 +0530

    feat(lib): add ability to cache form fields

--- src/lib/storage.ts
+++ src/lib/storage.ts
@@ -1,13 +1,13 @@
 /**
  * __Mutates__ the provided `obj` by applying `changes` at depth `keys`
- * @param {array} keys in the order of depth, give *empty* array or array with *single* element for flat objects,
+ * @param keys in the order of depth, give *empty* array or array with *single* element for flat objects,
  * the first key is considered as obj name and thus never utilized
- * @param {!Object} obj to __mutate__
- * @param {!Object} changes to make
+ * @param obj to __mutate__
+ * @param changes to apply
  *
  * @internal
  */
-export const updateObj = (keys, obj, changes) => {
+export const updateObj = (keys: string[], obj: object, changes: object) => {
   // todo figure out a better way without mutating function parameters.
   //       workaround could be to deepClone the `obj`, make changes, return the clone.
   if (keys.length > 1) {
@@ -15,7 +15,7 @@ export const updateObj = (keys, obj, changes) => {
     const traverse = [...keys];
     traverse.shift();
     // Leveraging the fact that javascript doesn't actually deepClone objects 😏.
-    const pointer = traverse.reduce((accumulator, currentValue) => {
+    const pointer = traverse.reduce((accumulator: Object, currentValue: any) => {
       if (accumulator[currentValue] === undefined) accumulator[currentValue] = {};
       return accumulator[currentValue];
     }, obj);
@@ -24,31 +24,30 @@ export const updateObj = (keys, obj, changes) => {
     });
   } else {
     Object.keys(changes).forEach(property => {
-      /* eslint-disable no-param-reassign */
       obj[property] = changes[property];
-      /* eslint-disable no-param-reassign */
     });
   }
 };
+
 /**
  *
- * @param {!String} name of the obj to retrieve
- * @returns {Promise<any>} the actual object if found, empty object otherwise
+ * @param name of the obj to retrieve
+ * @returns the actual object if found, empty object otherwise
  */
-const getStorageObj = async name => (await browser.storage.local.get())[name] || {};
+const getStorageObj = async (name: string): Promise<Object> => (await browser.storage.local.get())[name] || {};
 
 /**
  * Updates the properties of object found at `path` in local storage
  * by applying `changes`
  *
- * @param {String} path to the object to update separated by `.`
+ * @param path to the object to update separated by `.`
  *
- * @param {String|Object} changes object containing only changed properties
+ * @param changes object containing only changed properties
  *
  * @internal
  */
-export const updateStorageObj = async (path, changes) => {
-  const keys = path.split('.');
+export const updateStorageObj = async (path: string, changes: object) => {
+  const keys = path.startsWith('$$') ? [path] : path.split('.');
   const obj = await getStorageObj(keys[0]);
 
   updateObj(keys, obj, changes);
@@ -67,7 +66,7 @@ const scoreCache = {
    * @param {!Number}score
    * @example scoreCache.set('https://theAnswerToLife.Is', 42);
    */
-  set: async (url, score) =>
+  set: async (url: string, score: number) =>
     updateStorageObj('scoreCache', {
       [url]: score,
     }),
@@ -78,7 +77,7 @@ const scoreCache = {
    * @example scoreCache.get('https://theAnswerToLife.Is')
    * will return 42;
    */
-  get: async url => {
+  get: async (url: string): Promise<number | object> => {
     const scoreCaches = await getStorageObj('scoreCache');
     if (!url) return scoreCaches;
     return scoreCaches[url] || NaN;
@@ -86,14 +85,37 @@ const scoreCache = {
 };
 
 /**
- *
- * @param {!String} obj inside `config` can be one of `['settings','popup','content','background'] `
- * if not specified returns the whole config object
- * @param {!String} key property of `obj` to access
- * @param {any} defaultValue to return if nothing found
- * @returns {Promise<any|undefined>} value if found, `defaultValue` otherwise.
+ * formCache is a top-level object with keys as url & property as an object with fields.
  */
-const getConfig = async (obj, key, defaultValue = undefined) => {
+const formCache = {
+  /**
+   *
+   * @example formCache.set('https://theAnswerToLife.Is', {field1:'value'});
+   */
+  set: async (url: string, fields: Object) => updateStorageObj('$$' + url, fields),
+
+  /**
+   *
+   * @returns {Promise<Object>} fields if exists, `{}` otherwise
+   * @example formCache.get('https://theAnswerToLife.Is')
+   * will return {field1:'value'};
+   */
+  get: async (url: string): Promise<object> => {
+    const formCache: Object = await getStorageObj('$$' + url);
+
+    return formCache;
+  },
+};
+
+/**
+ *
+ * @param obj inside `config` can be one of `['settings','popup','content','background'] `
+ * if not specified returns the whole config object
+ * @param key property of `obj` to access
+ * @param defaultValue to return if nothing found
+ * @returns {Promise<any|undefined>} value if found, `defaultValue` if specified, `undefined` otherwise.
+ */
+const getConfig = async (obj: string, key: string, defaultValue: any = undefined): Promise<any | undefined> => {
   const config = await getStorageObj('config');
 
   // This will never happen! duh.. 🤦♂
@@ -114,26 +136,26 @@ const getConfig = async (obj, key, defaultValue = undefined) => {
 const config = {
   settings: {
     /**
-     * @param {!String} key
-     * @param {String|Object} value
+     *
+     * @example storage.config.settings.set ('settingName1', 'settingsValue1');
      */
-    set: async (key, value) =>
+    set: async (key: string, value: string) =>
       updateStorageObj('config.settings', {
         [key]: value,
       }),
     /**
-     * @param {!String} key to access
+     * @param key to access
      * @returns {Promise<any|undefined>} value if found, if `key` not specified returns the whole settings object,
      *  `undefined` otherwise.
      */
-    get: async key => getConfig('settings', key),
+    get: async (key: string): Promise<any | undefined> => getConfig('settings', key),
   },
   popup: {
     /**
      * @param {!String} key
      * @param {String|Object} value
      */
-    set: async (key, value) =>
+    set: async (key: string, value: string | object) =>
       updateStorageObj('config.popup', {
         [key]: value,
       }),
@@ -142,14 +164,14 @@ const config = {
      * @returns {Promise<any|undefined>} value if found, if `key` not specified returns the whole popup object,
      *  `undefined` otherwise.
      */
-    get: async key => getConfig('popup', key),
+    get: async (key: string): Promise<any | undefined> => getConfig('popup', key),
   },
   content: {
     /**
      * @param {!String} key
      * @param {String|Object} value
      */
-    set: async (key, value) =>
+    set: async (key: string, value: string | object) =>
       updateStorageObj('config.content', {
         [key]: value,
       }),
@@ -158,14 +180,14 @@ const config = {
      * @returns {Promise<any|undefined>} value if found, if `key` not specified returns the whole content object,
      *  `undefined` otherwise.
      */
-    get: async key => getConfig('content', key),
+    get: async (key: string): Promise<any | undefined> => getConfig('content', key),
   },
   background: {
     /**
      * @param {!String} key
      * @param {String|Object} value
      */
-    set: async (key, value) =>
+    set: async (key: string, value: string | object) =>
       updateStorageObj('config.background', {
         [key]: value,
       }),
@@ -174,7 +196,7 @@ const config = {
      * @returns {Promise<any|undefined>} value if found, if `key` not specified returns the whole background object,
      *  `undefined` otherwise.
      */
-    get: async key => getConfig('background', key),
+    get: async (key: string): Promise<any | undefined> => getConfig('background', key),
   },
 };
 
@@ -184,6 +206,7 @@ const config = {
  */
 const storage = {
   scoreCache,
+  formCache,
   config,
 };
 
