b95930c501c5af258e357846188759d53b0590ea
devkabiir <dev@kabiir.me>
2018-09-05 02:28:33 +0530

    fix(lib): processLinks was tring to iterate over NodeListOf

--- src/lib/processLinks.ts
+++ src/lib/processLinks.ts
@@ -1,22 +1,25 @@
 /**
  * @param {HTMLAnchorElement} e
  */
-function isValidLink(e) {
+function isValidLink(e: HTMLAnchorElement) {
   return (
     e.href.startsWith('https://') &&
     e.innerHTML.trim() &&
     e.innerText.trim() &&
+    // @ts-ignore
     !e.getAttribute('href').startsWith('#') &&
+    // @ts-ignore
     !e.getAttribute('href').startsWith('/')
   );
 }
+
 /**
  *
  * @param {HTMLAnchorElement} e
  * @param {Number} i
  * @param {HTMLAnchorElement[]} arr
  */
-function isUniqueLink(e, i, arr) {
+function isUniqueLink(e: HTMLAnchorElement, i: number, arr: HTMLAnchorElement[]) {
   for (let j = i + 1, len = arr.length; j < len; j += 1) {
     if (e.getAttribute('href') === arr[j].getAttribute('href')) return false;
   }
@@ -27,11 +30,15 @@ function isUniqueLink(e, i, arr) {
  * @param {function} callback to call with unique links
  * @returns {Promise<any>} return value of the callback.
  */
-export default async function processLinks(callback) {
+export default async function processLinks(callback: Function): Promise<any> {
   if (typeof callback !== 'function') return false;
 
   const anchorTags = document.getElementsByTagName('a');
 
-  const uniqueLinks = [...anchorTags.values()].filter((e, i, arr) => isValidLink(e) && isUniqueLink(e, i, arr));
+  const anchorTagsValues: any[] = [];
+
+  Array.from(anchorTags).forEach(e => anchorTagsValues.push(e));
+
+  const uniqueLinks = [...anchorTagsValues].filter((e, i, arr) => isValidLink(e) && isUniqueLink(e, i, arr));
   return callback(uniqueLinks);
 }
