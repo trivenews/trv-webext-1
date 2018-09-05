4843d5b28eae729333a1f6e93ecdadaebc7418cf
devkabiir <dev@kabiir.me>
2018-09-05 20:22:13 +0530

    fix(lib): contextMenu util was accessing browser.storage directly

--- src/lib/contextMenu.ts
+++ src/lib/contextMenu.ts
@@ -1,6 +1,7 @@
+import storage from './storage';
 const contextMenu = {
   register: async () => {
-    const showScoreOnLinks = !!(await browser.storage.local.get('showScoreOnLinks'));
+    const showScoreOnLinks = !!(await storage.config.content.get('showScoreOnLinks'));
     const itemType: browser.contextMenus.ItemType = 'checkbox';
     const pageAction: browser.contextMenus.ContextType = 'page_action';
     const browserAction: browser.contextMenus.ContextType = 'browser_action';
@@ -12,33 +13,30 @@ const contextMenu = {
         title: 'Show scores on links',
         contexts: [browserAction],
       },
-      {
-        checked: false,
-        id: 'alternateScoreColors',
-        type: itemType,
-        title: 'Use alternate score colors here',
-        contexts: [pageAction],
-      },
+      // {
+      //   checked: false,
+      //   id: 'alternateScoreColors',
+      //   type: itemType,
+      //   title: 'Use alternate score colors here',
+      //   contexts: [pageAction],
+      // },
     ];
 
     menuItems.forEach(item => browser.contextMenus.create(item));
   },
+
   handler: async (info, tab) => {
-    let alternateScoreColors;
+    // let alternateScoreColors;
     const menuId = info.menuItemId;
     switch (menuId) {
       case 'showScoreOnLinks':
-        await browser.storage.local.set({
-          showScoreOnLinks: info.checked,
-        });
+        await storage.config.content.set('showScoreOnLinks', info.checked);
         break;
-      case 'alternateScoreColors':
-        alternateScoreColors = (await browser.storage.local.get('alternateScoreColors')) || [];
-        alternateScoreColors.push(tab.url);
-        await browser.storage.local.set({
-          alternateScoreColors,
-        });
-        break;
+      // case 'alternateScoreColors':
+      //   alternateScoreColors = (await storage.config.content.get('alternateScoreColors')) || [];
+      //   alternateScoreColors.push(tab.url);
+      //   await storage.config.content.set('alternateScoreColors', alternateScoreColors);
+      //   break;
       default:
     }
   },
