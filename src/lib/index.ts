39867062006a54c81d94619b4dd3152edcfad625
devkabiir <dev@kabiir.me>
2018-09-05 20:19:14 +0530

    feat(lib): expose utilities in use from index.ts

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,14 @@
 import * as storageUtils from './storage';
 export const storage = storageUtils.default;
+
 import * as popupLinksUtils from './popupLinks';
 export const popupLinks = popupLinksUtils.default;
+
+import * as scoreCardComponentUtils from './scoreCardComponent';
+export const scoreCardComponent = scoreCardComponentUtils.default;
+
+import * as processLinksUtils from './processLinks';
+export const processLinks = processLinksUtils.default;
+
+import * as contextMenuUtils from './contextMenu';
+export const contextMenu = contextMenuUtils.default;
